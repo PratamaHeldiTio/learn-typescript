b12b427711bfe6d6aa1d1f1e06c1746fef943958
pratamahelditio <pratamahelditio@gmail.com>
2021-11-04 09:48:53 +0700

    perf: data type number and bolean

--- src/dataType.ts
+++ src/dataType.ts
@@ -1,5 +1,25 @@
+// penulisan const variableName: dataType = value
+// atau const variableName = value
+// jika tidak ada datatype maka akan otomatis menggunakan datatype pertama
+// contoh const name = 'heldi' sama dengan const name: string = 'heldi'
+
+
+
+// String
 let nama: string = 'heldi tio'
 
 // nama tidak bisa diisi integer karna type datanya string
 // nama = 20
-console.log(nama)
+
+//Number (int float, dll)
+let age: number = 20
+// error
+// age = '1/2'
+
+// Bolean
+let isTyping: boolean = true
+// error
+// isTyping = 'false'
+
+
+console.log(isTyping)
